43ff165bd3c2043639de73db41080c967dba7118
Vebjørn Nyvoll <vebjorn.nyvoll@gmail.com>
2024-04-18 18:37:20 +0200

    added primereact etc

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { PrimeReactProvider } from 'primereact/api';
+import { Provider, ReactReduxContext } from 'react-redux'
+import store from './services/store'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <PrimeReactProvider>
+      <Provider store={store}>
     <App />
+      </Provider>
+    </PrimeReactProvider>
+      
   </React.StrictMode>,
 )
